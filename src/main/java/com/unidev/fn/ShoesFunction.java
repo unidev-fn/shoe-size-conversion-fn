97103e439e45cffe721d659a1ba5687b50f40f74
Denis O <denis@universal-development.com>
2018-02-05 00:12:11 +0200

    Added Size matrix constants

--- src/main/java/com/unidev/fn/ShoesFunction.java
+++ src/main/java/com/unidev/fn/ShoesFunction.java
@@ -1,10 +1,37 @@
 package com.unidev.fn;
 
+/**
+ * Function for conversion of shoes size.
+ */
 public class ShoesFunction {
 
-    public String handleRequest(ShoesInput shoesInput) {
+    // US Euro UK  Inches CM
+    static final String SIZE_MATRIX[][] = {
+        {"4","35","2","8.1875","20.8"},
+        {"4.5","35","2.5","8.375","21.3"},
+        {"5","35-36","3","8.5","21.6"},
+        {"5.5","36","3.5","8.75","22.2"},
+        {"6","36-37","4","8.875","22.5"},
+        {"6.5","37","4.5","9.0625","23"},
+        {"7","37-38","5","9.25","23.5"},
+        {"7.5","38","5.5","9.375","23.8"},
+        {"8","38-39","6","9.5","24.1"},
+        {"8.5","39","6.5","9.6875","24.6"},
+        {"9","39-40","7","9.875","25.1"},
+        {"9.5","40","7.5","10","25.4"},
+        {"10","40-41","8","10.1875","25.9"},
+        {"10.5","41","8.5","10.3125","26.2"},
+        {"11","41-42","9","10.5","26.7"},
+        {"11.5","42","9.5","10.6875","27.1"},
+        {"12","42-43","10","10.875","27.6"},
+    };
 
-        return shoesInput + "";
+
+    public CalculationOutput handleRequest(CalculationInput calculationInput) {
+
+        CalculationOutput calculationOutput = new CalculationOutput();
+
+        return calculationOutput;
     }
 
 }
