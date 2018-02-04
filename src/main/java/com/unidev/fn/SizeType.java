c4bd49679c4b0571d3755e47a3d629ca48aa0554
Denis O <denis@universal-development.com>
2018-02-05 00:21:37 +0200

    Added size type index

--- src/main/java/com/unidev/fn/SizeType.java
+++ src/main/java/com/unidev/fn/SizeType.java
@@ -2,6 +2,15 @@ package com.unidev.fn;
 
 public enum SizeType {
 
-    US, Europe, UK, Inc, CM
+    US(0), Europe(1), UK(2), Inc(3), CM(4);
 
+    private int index;
+
+    SizeType(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
 }
