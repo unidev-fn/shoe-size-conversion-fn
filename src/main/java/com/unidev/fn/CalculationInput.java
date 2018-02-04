4c36aefb201cdbcdb13c7ea092e989e6b1e256cc
Denis O <denis@universal-development.com>
2018-02-05 00:11:51 +0200

    Calculation input and output

--- src/main/java/com/unidev/fn/CalculationInput.java
+++ src/main/java/com/unidev/fn/CalculationInput.java
@@ -1,13 +1,13 @@
 package com.unidev.fn;
 
-public class ShoesInput {
+public class CalculationInput {
 
     public String size;
     public SizeType type;
 
     @Override
     public String toString() {
-        final StringBuffer sb = new StringBuffer("ShoesInput{");
+        final StringBuffer sb = new StringBuffer("CalculationInput{");
         sb.append("size='").append(size).append('\'');
         sb.append(", type='").append(type).append('\'');
         sb.append('}');
---
+++ src/main/java/com/unidev/fn/CalculationOutput.java
@@ -0,0 +1,11 @@
+package com.unidev.fn;
+
+public class CalculationOutput {
+
+    public String usSize;
+    public String euroSize;
+    public String ukSize;
+    public String inc;
+    public String cm;
+
+}
